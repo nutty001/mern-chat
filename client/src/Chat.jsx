30d98d6384af36e13f8d353bca516168397ef43d
nutty001 <90544250+nutty001@users.noreply.github.com>
2025-03-11 16:40:39 +0530

    backend server link added to frontend

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
   }, [selectedUserId]);
 
   function connectToWs() {
-    const ws = new WebSocket(`ws://localhost:4040`);
+    const ws = new WebSocket(`ws://mern-chat-backend-zrfi.onrender.com`);
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
@@ -145,12 +145,12 @@ export default function Chat() {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              class="w-4 h-4"
+              className="w-4 h-4"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
 
