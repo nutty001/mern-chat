f8f1937bc3e2d279a656aae0f80bcdd8f2e2014a
nutty001 <90544250+nutty001@users.noreply.github.com>
2025-03-18 13:29:17 +0530

    axios link in env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "https://mern-chat-backend-zrfi.onrender.com";
+  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
