b7902b19dfadb6e0568ca4e4ca39a456e20d2285
nutty001 <90544250+nutty001@users.noreply.github.com>
2025-03-20 19:07:04 +0530

    cookie fixed

--- api/index.js
+++ api/index.js
@@ -92,7 +92,7 @@ app.post('/login', async (req,res) => {
     const passOk = bcrypt.compareSync(password,foundUser.password);
     if(passOk){
       jwt.sign({userId:foundUser._id,username},jwtSecret,{},(err,token)=>{
-        res.cookie('token',token).json({
+        res.cookie('token',token,{sameSite: 'none'}).json({
           id:foundUser._id,
         });
       });
